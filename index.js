e74e62ef2232a91f2c8561a0fc11ae98f8678750
silan.wu <silan.wu@liulishuo.com>
2020-11-15 23:41:08 +0800

    async await，洋葱模型

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 import Koa from 'koa' //引入
 const app = new Koa()//实例化
 
-app.use((ctx) => {
+app.use(async (ctx, next) => {
     ctx.response.body = 'hello world'
+    console.log('进入中间件1')
+    await next()
+    console.log('退出中间件1')
+})
+app.use(async (ctx, next) => {
+    ctx.response.body = 'hello world'
+    console.log('进入中间件2')
+    await next()
+    console.log('退出中间件2')
+})
+app.use(async (ctx, next) => {
+    ctx.response.body = 'hello world'
+    console.log('进入中间件3')
+    await next()
+    console.log('退出中间件3')
+})
+app.use(async (ctx, next) => {
+    ctx.response.body = 'hello world'
+    console.log('进入中间件4')
+    console.log('退出中间件4')
 })
 
-app.listen(3003)
+app.listen(3003)//程序运行端口
