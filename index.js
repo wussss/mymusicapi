7ee0ed9cf9ad13b54de52d709c36cbcd2d0483b2
silan.wu <silan.wu@liulishuo.com>
2020-11-17 10:49:45 +0800

    get put post delete

--- index.js
+++ index.js
@@ -3,15 +3,30 @@ import Router from 'koa-router'
 const app = new Koa()//实例化
 const userRouter = new Router({ prefix: '/users' }) //实例化并加上前缀
 
-userRouter.get('/:id',(ctx) => {
-    ctx.body = `这是用户 ${ctx.params.id}`
+userRouter.get('/:id', (ctx) => {
+    ctx.body = {
+        method: '查询',
+        name: ctx.params.id,
+        age: 19
+    }
 })
-userRouter.get('/', (ctx) => {
-    ctx.body = '请求用户列表1'
+userRouter.post('/:id', (ctx) => {
+    ctx.body = {
+        method: '新建对象',
+        name: ctx.params.id,
+        age: 19
+    }
 })
-userRouter.options('/',(ctx) => {
-    ctx.header.allowedMethods = 'get,options'
+userRouter.delete('/:id', (ctx) => {
+    ctx.status = 204
 })
+userRouter.put('/:id', (ctx) => {
+    ctx.body = {
+        method: '修改用户名',
+        name: 'wuslan',
+    }
+})
+
 app.use(userRouter.routes())//注册到app里
 app.use(userRouter.allowedMethods()) //支持options请求
 
